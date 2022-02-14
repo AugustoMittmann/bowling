251e1fae9b78e7ac46348c1a5ac8da8fd3db18f1
Augusto Mittmann Note <gutomittmann10@gmail.com>
2022-02-14 10:37:33 -0300

    a

--- bowling/src/App.js
+++ bowling/src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 
 function App() {
   const arrayFrames = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] //display
-
+  
   const [frame, setFrame] = useState(0)
   const [round, setRound] = useState(1)
   const [firstNumber, setFirstNumber] = useState([])
   const [secondNumber, setSecondNumber] = useState([])
   const [result, setResult] = useState([])
-  const [resultado, setResultado] = useState([0])
-
+  const [resultado, setResultado] = useState([])
+  const arrayy = []
+  
 
   function click(buttonClick) {
     if(round === 1) {
@@ -18,34 +19,34 @@ function App() {
         setFirstNumber(firstNumber => [...firstNumber, ''])
         setSecondNumber(secondNumber => [...secondNumber, buttonClick])
         setFrame(frame+1)
-        
-        if(result.length === 0){
-          setResult(result => [...result, [buttonClick]])
-        } else {
-          console.log(firstNumber[frame], buttonClick, parseInt(result[result.length-1]));
-          setResult(result => [...result, [buttonClick + parseInt(result[result.length-1])]])
-        }
+        resultados(0)
       } else {
         setFirstNumber(firstNumber => [...firstNumber, buttonClick])
         setRound(2)
       }
     }
-
-
     if(round === 2) {
       setSecondNumber(secondNumber => [...secondNumber, buttonClick])
       setFrame(frame+1)
-      if(result.length === 0){
-        setResult(result => [...result, [firstNumber[frame] + buttonClick]])
-      } else {
-        console.log(firstNumber[frame], buttonClick, parseInt(result[result.length-1]));
-        setResult(result => [...result, [firstNumber[frame] + buttonClick + parseInt(result[result.length-1])]])
-      }
       setRound(1)
     }
+    setResult(resultados(buttonClick))
   }
+  function resultados(buttonClick) {
+    arrayy.push(buttonClick)
+    console.log(arrayy);
+    return arrayy
 
-
+    if(resultado % 2 !== 0) {
+      if(resultado.length === 0){
+        setResult(result => [...result, [buttonClick]])
+      } else {
+        setResult(result => [...result, [buttonClick + parseInt(result[result.length-1])]])
+      }
+    }
+  }
+    
+    
   return (
     <>
     <section className='container'>
